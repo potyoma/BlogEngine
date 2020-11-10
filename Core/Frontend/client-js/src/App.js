ca0f1c71690ddb8d5e9ebccdb5b9b7e2b7c7c6cf
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-11-10 10:16:17 +0300

    Updated docs for api

--- Core/Frontend/client-js/src/App.js
+++ Core/Frontend/client-js/src/App.js
@@ -7,22 +7,28 @@ function App() {
     blogs: [],
   };
 
-  props.blogs = getProps();
+  props.blogs = getRequest()
 
   return (
     <div>
       <h1>Welcome!</h1>
+      <Main blogs={props.blogs} />
     </div>
   );
 }
 
-function getProps() {
-  let result;
-  fetch("https://blog-engine-api.herokuapp.com/api/blogs", { method: "GET" }).then(
-    (res) => (result = res)
-  );
-  console.log(result);
-  return result;
+function getRequest() {
+  const url = "https://blog-engine-api.herokuapp.com/api/blogs"
+  let result
+  try {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => result = data)
+    return result
+  } catch (e) {
+    throw new Error("Error: ", e.message)
+  }
 }
 
 export default App;
+ 
