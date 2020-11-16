4bca50af2813c2a9b4dd2419790f38cfa513a15e
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-11-16 13:48:14 +0300

    Let client side rest

--- Core/Server/WebApp/Program.cs
+++ Core/Server/WebApp/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BlogEngine
+namespace WebApp
 {
     public class Program
     {
