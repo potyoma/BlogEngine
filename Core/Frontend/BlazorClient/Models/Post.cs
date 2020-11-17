223873662189f1d6c453d38d0172b96d2a0a0259
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-11-17 16:15:26 +0300

    Added list of posts | Working with routing

---
+++ Core/Frontend/BlazorClient/Models/Post.cs
@@ -0,0 +1,11 @@
+namespace BlazorClient.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string PostName { get; set; }
+        public string BlogUrl { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+    }
+}
