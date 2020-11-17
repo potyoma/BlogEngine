8753e9b7e97168a18f3b5d79f7e69760e26da6cc
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-11-17 09:41:03 +0300

    It shows blogs list

---
+++ Core/Frontend/BlazorClient/Models/Blog.cs
@@ -0,0 +1,11 @@
+namespace BlazorClient.Models
+{
+    public class Blog
+    {
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string BlogUrl { get; set; }
+    }
+}
